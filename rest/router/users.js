6b2320ca7cdce56310aa84fe831ca45774f69339
Dong Handii <handyrick.tampon@gmail.com>
2022-03-31 01:04:41 +0800

    Updated code for validation

--- rest/router/users.js
+++ rest/router/users.js
@@ -1,18 +1,29 @@
 const express = require('express')
 const router = express.Router()
 const Usermod = require('../models/usermod')
+const {regValidation, loginValidation} = require('../validation')
 
 router.get('/',(req,res)=>{
     res.send('this is userz')
 })
 
 router.post('/', async (req,res)=>{
-    const user = new Usermod({
-        name:req.body.name,
-        password:req.body.password,
-        email:req.body.email
-    })
+
     try{
+        //general validation
+        const valResult = await regValidation.validateAsync(req.body);
+        console.log(valResult);
+        
+        //existing email validation
+        const emailExist = await Usermod.findOne({email:req.body.email})
+        if(emailExist) return res.status(400).send('User email already in use.')
+    
+        const user = new Usermod({
+            name:req.body.name,
+            password:req.body.password,
+            email:req.body.email
+        })
+    
         const addUser = await user.save()
         res.status(201).json(addUser)
     }catch(error){
